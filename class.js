3172257b983e7f69acb42b35e67474aad86e098d
Geovanna <geovanna_muller@estudante.sesisenai.org.br>
2024-03-13 09:26:10 -0300

    Iniciando atual_monstro e point

--- class.js
+++ class.js
@@ -16,4 +16,22 @@ class Obj{
         img.src = this.a 
         des.drawImage(img,this.x,this.y,this.w,this.h)
     }
+    atual_monstro(){
+        this.x += this.comand
+        if(this.x <=2){
+            this.x = 2
+        }else if(this.x >= 416){
+            this.x = 416
+        }
+    }
+    point(objeto){
+        if((objeto.y>=680)&&(objeto.y <= 684)){
+            return true
+        }else{
+            false
+        }
+    }
+   
 }
+
+
