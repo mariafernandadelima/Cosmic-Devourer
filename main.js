fa04dce86d2d44db0a49a64a5c3b1bd45f308e61
Maria <maria_f_dias@estudante.sesisenai.org.br>
2024-03-20 08:11:22 -0300

    Iniciando pontuação

--- main.js
+++ main.js
@@ -17,27 +17,23 @@ som2.volume = 0.7
 
 let jogar = true
 
-let grupo_bomba = []
 let lixo_espacial = []
-let bomba
 let lixo = {
     time1: 0,
     time2: 0,
     time3: 0,
     time4: 0,
-    time5:0,
 
     crialixo(){
         this.time1 += 1
         this.time2 += 1
         this.time3 += 1
         this.time4 += 1
-        this.time5 += 1
         let pos_x = (Math.random() * (438 - 2 +1)+2)
         let pos_x2 = (Math.random() * (438 - 2 +1)+2)
         let pos_x3 = (Math.random() * (438 - 2 +1)+2)
         let pos_x4 = (Math.random() * (438 - 2 +1)+2)
-        let pos_x5 = (Math.random() * (438 - 2 +1)+2)
+
         if(this.time1 >=60){
             this.time1 = 0
             lixo_espacial.push(new Lixo(pos_x,-200,50,50,'./assets/img/lixo.png'))
@@ -56,11 +52,6 @@ let lixo = {
         if(this.time4 >=135){
             this.time4 = 0
             lixo_espacial.push(new Lixo(pos_x4,-500,50,50,'./assets/img/maca.png'))
-            console.log(lixo_espacial)
-        }
-        if(this.time5 >=135){
-            this.time5 = 0
-            lixo_espacial.push(new Lixo(pos_x5,-500,50,50,'./assets/img/bomba.png'))
             console.log(lixo_espacial)
         }
     },
@@ -119,7 +110,8 @@ function colisao(){
     lixo_espacial.forEach((bomba)=>{
         if(monstro.colid(bomba)){
             lixo_espacial.splice(lixo_espacial.indexOf(bomba), 1)
-            monstro.vida -=1
+            monstro.pts +=1
+            if(monstro.vida == 0){jogar = false}
         }
     })
 }
